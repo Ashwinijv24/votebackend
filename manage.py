b4eeffd7205095e03f9b283e796282e5b0efb38f
Ashwini J <165980983+Ashwinijv24@users.noreply.github.com>
2026-04-05 11:58:50 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VoteExpress.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        raise
+    except ImportError as exc:
+        raise exc
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
