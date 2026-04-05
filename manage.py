31b3e2761a42798d9f4bd6e693f5fddb132ed68f
Ashwini J <165980983+Ashwinijv24@users.noreply.github.com>
2026-04-05 12:17:55 +0530

    Refactor import statement for Django management

--- manage.py
+++ manage.py
@@ -4,10 +4,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VoteExpress.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise exc
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
